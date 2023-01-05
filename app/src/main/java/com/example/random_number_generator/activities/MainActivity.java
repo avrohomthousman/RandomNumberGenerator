b2dfe87dc27080b5d2d40f5b709394fe8cbafbd8
avrohomthousman <ahousman@student.touro.edu>
2023-01-05 14:20:04 -0500

    made fab action listener call model

--- app/src/main/java/com/example/random_number_generator/activities/MainActivity.java
+++ app/src/main/java/com/example/random_number_generator/activities/MainActivity.java
@@ -13,6 +13,7 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.util.Log;
 import android.view.View;
 
 import androidx.navigation.NavController;
@@ -79,11 +80,66 @@ public class MainActivity extends AppCompatActivity {
     private void setupFab() {
         binding.fabFile.fab.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                String from = binding.contentMain.minInputSection.minimumValue.getText().toString();;
+                String to = binding.contentMain.maxInputSection.maximumValue.getText().toString();;
+
+
+
+                if(!allDataHasBeenEntered(from, to)){
+                    makeErrorSnackBar(v, "Please make sure you entered all the required data");
+                    return;
+                }
+
+
+                int fromAsInt;
+                int toAsInt;
+                try{
+                    fromAsInt = Integer.parseInt(from);
+                    toAsInt = Integer.parseInt(to);
+                }
+                catch (NumberFormatException e){
+                    Log.println(Log.ERROR, "User Error", "Could not parse user entry to int");
+
+                    makeErrorSnackBar(v, "Please enter only valid numbers");
+                    return;
+                }
+
+
+                mRandomNumber.setFromTo(fromAsInt, toAsInt);
+                int result = mRandomNumber.getCurrentRandomNumber();
+                mNumberHistory.add(result);
+
+                binding.contentMain.output.setText(  String.valueOf(result)  );
             }
         });
+    }
+
+    /**
+     * Checks that all the specified strings have been initailized to a non null
+     * String with a length greater than zero. I made it general so it can support
+     * any number if strings.
+     *
+     * @param data the strings to test.
+     * @return true if all the strings have been initailized correctly, and false otherwise.
+     */
+    private boolean allDataHasBeenEntered(String... data){
+        for(String item : data){
+            if(item == null || item.length() == 0){
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+
+    /**
+     * Displays a snackBar with the specified error message.
+     */
+    private void makeErrorSnackBar(View parent, String message){
+        Snackbar.make(parent, message, Snackbar.LENGTH_LONG)
+                .show();
     }
 
 
